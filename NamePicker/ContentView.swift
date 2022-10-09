895f21f23513ce25274ce6f7dc1f83f97bf7c356
Thomas Andrew Martin <47917727+Dev0dyssey@users.noreply.github.com>
2022-10-09 10:40:15 +0100

    Update ContentView.swift

--- NamePicker/ContentView.swift
+++ NamePicker/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    let names = ["Lucia", "Guy", "Ruth", "Nathan", "Santosh", "Jackson", "Chibu", "Agnel", "Samora", "Amy", "Maz", "Jon"]
+    let columns = [ GridItem(.adaptive(minimum: 100)) ]
+    @State private var selectedName = ""
+    @State private var namePicked = false
+    func pickName() {
+        selectedName = names.randomElement() ?? "John Doe"
+        namePicked = true;
+        print(selectedName)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.purple, .white], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("Tap to choose name")
+                    .font(.largeTitle.weight(.semibold))
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(names, id: \.self) {
+                        item in
+                        Text(item)
+                            .padding()
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(.purple, lineWidth: 3)
+                                    .frame(width: 100)
+                            )
+                    }
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button("Choose name") {
+                    pickName()
+                }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.cyan)
+                Spacer()
+            }
         }
-        .padding()
+        .alert(selectedName, isPresented: $namePicked) {
+            Button("Close", role: .cancel) {}
+        } message: {
+            Text("Is the lucky winner")
+        }
     }
 }
 
