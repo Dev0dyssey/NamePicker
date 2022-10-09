9eb0264c88b30db9e2f09ea5b0e4bcedeb8db639
Thomas Andrew Martin <47917727+Dev0dyssey@users.noreply.github.com>
2022-10-09 09:39:50 +0100

    Initial Commit

---
+++ NamePicker/NamePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamePickerApp.swift
+//  NamePicker
+//
+//  Created by Thomas Martin on 09/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct NamePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
